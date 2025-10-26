9542380dca63362634652ebac5805a58b496d3ae
Bianca <bmcristin@gmail.com>
2025-10-26 20:51:58 -0300

    Agregado Metodo buscarJugadorId y modificacion de atributos(profesion y casa) y sus getters y setters

--- src/main/java/org/example/model/Jugadores/Jugador.java
+++ src/main/java/org/example/model/Jugadores/Jugador.java
@@ -2,22 +2,26 @@ package org.example.model.Jugadores;
 
 import org.example.bd.ConexionBD;
 import org.example.model.NPCs.NPC;
+import org.example.model.Piezas.Cartas.CartaAzul;
+import org.example.model.Piezas.Cartas.CartaNaranja;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Jugador {
     private int id_jugador;
     private String nombre;
-    private int profesion;
+    private CartaAzul profesion;
     private int patrimonio;
     private ArrayList<NPC> familia;
     private int posicion;
     private int estado_civil; // 0/1 porque sqlite no tiene booleano
     private int hijos;
     private int deuda;
+    private CartaNaranja casa;
 
     private static final String url_dinamica ="jdbc:sqlite:BD_LIFE_DINAMIC.sqlite";
     // ==========================
@@ -27,7 +31,7 @@ public class Jugador {
     public Jugador(int id, String nombre) {
         this.id_jugador = id;
         this.nombre = nombre;
-        this.profesion = 0;
+        this.profesion = null;
         this.patrimonio = 0;
         this.familia = new ArrayList<>();
         this.posicion = 0;
@@ -39,6 +43,8 @@ public class Jugador {
     public Jugador(String nombre) {
         this(0, nombre);
     }
+
+    public  Jugador(){};
 
     // ==========================
     // METODOS PROPIOS
@@ -70,8 +76,8 @@ public class Jugador {
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public int getProfesion() { return profesion; }
-    public void setProfesion(int profesion) { this.profesion = profesion; }
+    public CartaAzul getProfesion() { return profesion; }
+    public void setProfesion(CartaAzul profesion) { this.profesion = profesion; }
 
     public int getPatrimonio() { return patrimonio; }
     public void setPatrimonio(int patrimonio) { this.patrimonio = patrimonio; }
@@ -90,6 +96,9 @@ public class Jugador {
 
     public int getDeuda() { return deuda; }
     public void setDeuda(int deuda) { this.deuda = deuda; }
+
+    public CartaNaranja getCasa() {return casa;}
+    public void setCasa(CartaNaranja casa) {this.casa = casa;}
 
     // ==========================
     // METODOS DE BD
@@ -124,9 +133,6 @@ public class Jugador {
             case "nombre":
                 sql = "UPDATE jugador SET nombre=? WHERE id_jugador=?";
                 break;
-            case "profesion":
-                sql = "UPDATE jugador SET profesion=? WHERE id_jugador=?";
-                break;
             default:
                 System.err.println("Campo no válido para tipo String: " + campo);
                 return false;
@@ -151,6 +157,7 @@ public class Jugador {
     public boolean actualizar(String campo, int nuevoValor) {
         String sql;
         switch (campo) {
+            case "profesion": sql = "UPDATE jugador SET profesion=? WHERE id_jugador=?";break;
             case "patrimonio": sql = "UPDATE jugador SET patrimonio=? WHERE id_jugador=?"; break;
             case "estado_civil": sql = "UPDATE jugador SET estado_civil=? WHERE id_jugador=?"; break;
             case "hijos": sql = "UPDATE jugador SET hijos=? WHERE id_jugador=?"; break;
@@ -185,7 +192,50 @@ public class Jugador {
         }
     }
 
-    /** Busca un jugador por id en la BD y actualiza el objeto actual */
+    /** Busca un jugador por id en la BD y actualiza el objeto actual
+     * Para profesion y casa utiliza los campos profesion y objeto que contienen ids que refieren a los ids de las tablas que les corresponden
+     * */
+    public static Jugador buscarJugadorPorId(int idJugador) {
+        String sql = "SELECT * FROM jugador WHERE id_jugador = ?";
+        Jugador jugador = null;
+
+        try (Connection conn = new ConexionBD(url_dinamica).getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, idJugador);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                jugador = new Jugador();
+                jugador.setId(rs.getInt("id_jugador"));
+                jugador.setNombre(rs.getString("nombre"));
+                jugador.setPatrimonio(rs.getInt("patrimonio"));
+                jugador.setPosicion(rs.getInt("patrimonio"));
+                jugador.setEstado_civil(rs.getInt("estado_civil"));
+                jugador.setHijos(rs.getInt("hijos"));
+                jugador.setDeuda(rs.getInt("deudas"));
+
+
+                // Obtenemos los id de profesion y casa
+                int idProfesion = rs.getInt("id_profesion");
+                int idCasa = rs.getInt("id_casa");
+
+                // Creo objetos a partir de esos id con los metodos de cada carta y los asigno
+                CartaAzul cartaAzul = new CartaAzul();
+                CartaAzul profesion = cartaAzul.buscarCartaId(idProfesion);
+                CartaNaranja cartaNaranja = new CartaNaranja();
+                CartaNaranja casa = cartaNaranja.buscarCartaId(idCasa);
+
+                jugador.setProfesion(profesion);
+                jugador.setCasa(casa);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al buscar jugador: " + e.getMessage());
+        }
+
+        return jugador;
+    }
 
 
 }
