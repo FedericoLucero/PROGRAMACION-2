681febc523d18bd0943fe3f63090b5507cebed95
Bianca <bmcristin@gmail.com>
2025-10-27 14:45:25 -0300

    Agregado clase ruleta giratoria

--- src/main/java/org/example/MainTest2.java
+++ src/main/java/org/example/MainTest2.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import org.example.model.Jugadores.Jugador;
-import org.example.model.Piezas.Cartas.CartaAzul;
-import org.example.model.Piezas.Cartas.CartaNaranja;
-import org.example.model.Piezas.Tablero;
-import org.example.utils.UserInput;
+import org.example.GUI.GirarRuleta;
 
-import java.util.List;
+import javax.swing.*;
 
 public class MainTest2 {
 
@@ -21,30 +17,37 @@ public class MainTest2 {
 //        CartaAzul profesion1 = CartaAzul.buscarCartaId(2);
 //        System.out.println(profesion1.getTitulo());
         //test uso accion casilla azul
-        int posicion = 85;
-        Tablero t1 = new Tablero();
-        System.out.println(t1.getCantCasillas());
-        int nivel = t1.obtenerTercio(posicion);
-        CartaAzul profesion = new CartaAzul();
-        List<Integer> ids = profesion.obtenerRandom(nivel);
-        for (int id: ids){
-            System.out.println(id);
-        }
-        System.out.println(ids.get(1));
-        boolean jugarDeNuevo;
-        do {    // bucle "voler a jugar"
-
-            // metodo que pide la cantidad de jugadores (min 1, max 4)
-            //int cantJugadores = UserInput.getInt("Ingrese cantidad de jugadores ",1,4);
-
-            // objeto principal donde se realiza la partida
-            Juego juego = new Juego();
-
-            // metodo donde se realiza la partida
-            jugarDeNuevo = juego.jugar();
-
-        } while (jugarDeNuevo);
-        System.out.println("=== JUEGO TERMINADO ===");
+//        int posicion = 85;
+//        Tablero t1 = new Tablero();
+//        System.out.println(t1.getCantCasillas());
+//        int nivel = t1.obtenerTercio(posicion);
+//        CartaAzul profesion = new CartaAzul();
+//        List<Integer> ids = profesion.obtenerRandom(nivel);
+//        for (int id: ids){
+//            System.out.println(id);
+//        }
+//        System.out.println(ids.get(1));
+//        boolean jugarDeNuevo;
+//        do {    // bucle "voler a jugar"
+//
+//            // metodo que pide la cantidad de jugadores (min 1, max 4)
+//            //int cantJugadores = UserInput.getInt("Ingrese cantidad de jugadores ",1,4);
+//
+//            // objeto principal donde se realiza la partida
+//            Juego juego = new Juego();
+//
+//            // metodo donde se realiza la partida
+//            jugarDeNuevo = juego.jugar();
+//
+//        } while (jugarDeNuevo);
+//        System.out.println("=== JUEGO TERMINADO ===");
+        GirarRuleta tr = new GirarRuleta(); // crear una instancia
+       // System.out.println(tr.valor);
+        JFrame frame = new JFrame("Rotar Imagen");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.add(new GirarRuleta());
+        frame.setVisible(true);
     }
 
 }
