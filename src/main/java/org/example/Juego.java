e3e4bc35d56bb20809f6770027798beeee65a4b0
FedericoLucero <federicolucero2002@gmail.com>
2025-10-27 17:31:43 -0300

    ventanaCarta que contiene todas las ventanas de cartas

--- src/main/java/org/example/Juego.java
+++ src/main/java/org/example/Juego.java
@@ -22,37 +22,22 @@ public class Juego {
 
     private Jugador[] jugadores;
     private Tablero tablero = new Tablero();
-
     private int cantJugadores;
     private int cantMovimientosActuales = 0;
     private int finJuego;
-
     private VentanaJuego ventanaJuego;
-
     UserInput ui = new UserInput();
-    // ==========================
-    // CONSTRUCTORES
-    // ==========================
 
     public Juego() {}
 
-    // ==========================
-    // MÉTODOS LÓGICOS
-    // ==========================
-
-    /**
-     * Metodo donde se realiza tod0 el juego
-     * @return true volver a jugar o false no volver a jugar
-     */
     public boolean jugar() {
 
         inicializar(); // inicializa atributos del juego
 
         // variable para finalizar el bucle
         boolean seguirPartida = true;
-        do { // bucle principal de la partida
 
-            // metodo que cambia el jugador
+        do { // bucle principal de la partida
             Jugador jugadorTurno = cambiarJugador(cantMovimientosActuales);
 
             // if que verifica que el jugador no esté en el final
@@ -74,90 +59,59 @@ public class Juego {
                     switch (Casilla.obtenerColorCasillaPorId(siguientePosicion)) {
 
                         case "amarilla":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla AMARILLO"," Posición: " + siguientePosicion , PantallaColor.AMARILLO);
-                            accionAmarilla(jugadorTurno,nivel);
+                            ventanaJuego.setDescripcion("Caiste en una casilla AMARILLO", " Posición: " + siguientePosicion, PantallaColor.AMARILLO);
+                            accionAmarilla(jugadorTurno, nivel);
                             break;
-
                         case "azul":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla AZUL"," Posición: "+ siguientePosicion, PantallaColor.AZUL);
-                            accionAzul(jugadorTurno,nivel);
+                            ventanaJuego.setDescripcion("Caiste en una casilla AZUL", " Posición: " + siguientePosicion, PantallaColor.AZUL);
+                            accionAzul(jugadorTurno, nivel);
                             break;
-
                         case "roja":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla ROJA"," Posición: "+ siguientePosicion, PantallaColor.ROJO);
-                            accionRoja();
+                            ventanaJuego.setDescripcion("Caiste en una casilla ROJA", " Posición: " + siguientePosicion, PantallaColor.ROJO);
+                            accionRoja(jugadorTurno);
                             break;
-
                         case "verde":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla VERDE"," Posición: "+ siguientePosicion, PantallaColor.VERDE);
+                            ventanaJuego.setDescripcion("Caiste en una casilla VERDE", " Posición: " + siguientePosicion, PantallaColor.VERDE);
                             accionVerde(jugadorTurno);
                             break;
-
                         case "rosa":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla ROSA"," Posición: " + siguientePosicion, PantallaColor.ROSA);
+                            ventanaJuego.setDescripcion("Caiste en una casilla ROSA", " Posición: " + siguientePosicion, PantallaColor.ROSA);
                             accionRosa();
                             break;
-
                         case "naranja":
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla NARANJA"," Posición: " + siguientePosicion, PantallaColor.NARANJA);
-                            accionNaranja(jugadorTurno,nivel);
+                            ventanaJuego.setDescripcion("Caiste en una casilla NARANJA", " Posición: " + siguientePosicion, PantallaColor.NARANJA);
+                            accionNaranja(jugadorTurno, nivel);
                             break;
-
-                        // =================
-                        // caso de STOPS
-                        // acción especial (las casiilas de stop son siempre fijas en el tablero)
-                        // =================
-
-                        case "stopAzul": // casilla 3
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla STOP AZUL"," Posición: " + siguientePosicion, PantallaColor.BLANCO);
-                            accionAzul(jugadorTurno,nivel);
+                        case "stopAzul":
+                            ventanaJuego.setDescripcion("Caiste en una casilla STOP AZUL", " Posición: " + siguientePosicion, PantallaColor.BLANCO);
+                            accionAzul(jugadorTurno, nivel);
                             break;
-
-                        case "stopRosa": // casilla 28
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla STOP ROSA"," Posición: " + siguientePosicion, PantallaColor.BLANCO);
+                        case "stopRosa":
+                            ventanaJuego.setDescripcion("Caiste en una casilla STOP ROSA", " Posición: " + siguientePosicion, PantallaColor.BLANCO);
                             accionRosa();
                             break;
-
-                        case "stopNaranja": // casilla 40
-
-                            ventanaJuego.setDescripcion("Caiste en una casilla STOP NARANJA"," Posición: " + siguientePosicion, PantallaColor.BLANCO);
-                            accionNaranja(jugadorTurno,nivel);
+                        case "stopNaranja":
+                            ventanaJuego.setDescripcion("Caiste en una casilla STOP NARANJA", " Posición: " + siguientePosicion, PantallaColor.BLANCO);
+                            accionNaranja(jugadorTurno, nivel);
                             break;
-
                         default:
                             System.out.println(GRIS + "Color no reconocido: Posición: " + siguientePosicion + RESET);
                             break;
                     }
-
-                } else {  // la posicion del jugador es mayor o igual a el final
-
-                    ventanaJuego.setDescripcion("llegaste al final"," Posición: " + siguientePosicion, PantallaColor.BLANCO);
+                } else {
+                    ventanaJuego.setDescripcion("llegaste al final", " Posición: " + siguientePosicion, PantallaColor.BLANCO);
                     jugadorTurno.moverJugador(tablero.getCantCasillas());
                     finJuego -= 1;
                 }
 
-                // actualizamos informaciondel jugador en la ventana
-                ventanaJuego.actualizarValoresJugador(jugadorTurno.getId(),jugadorTurno.getPosicion(), jugadorTurno.getPatrimonio(), jugadorTurno.getCantidadCasas(), jugadorTurno.getCantidadFamiliares());
+                ventanaJuego.actualizarValoresJugador(jugadorTurno.getId(), jugadorTurno.getPosicion(),
+                        jugadorTurno.getPatrimonio(), jugadorTurno.getCantidadCasas(), jugadorTurno.getCantidadFamiliares());
 
-                // if que verifica el final de la partida
-                if (finJuego == 0){ // verificar el fin real del juego (cuando todos los jugadores llegan a la meta)
-
-                    // todo verificar ganador de la partida
-
+                if (finJuego == 0) {
                     seguirPartida = false;
                 }
             }
-
-
-            cantMovimientosActuales += 1;
+            cantMovimientosActuales++;
         } while (seguirPartida);
 
 
@@ -166,13 +120,13 @@ public class Juego {
 
         // cierra la ventanaJuego del juego
         ventanaJuego.dispose();
+
         //vaciar la bd
         borrarDatosBDDinamica();
-
         return fin;
     }
 
-    public void inicializar(){
+    public void inicializar() {
         this.cantJugadores = pedirCantidadJugadores();
         this.finJuego = cantJugadores;
         this.jugadores = pedirNombresJugadores(cantJugadores);
@@ -182,134 +136,103 @@ public class Juego {
         }
     }
 
-    /**
-     * Cambia el turno al jugador correspondiente según el número de movimiento actual.
-     * @param cantMovActual número total de movimientos realizados hasta el momento.
-     * @return el {Jugador} que tiene el turno actual.
-     */
     public Jugador cambiarJugador(int cantMovActual) {
-
-        Jugador jugadorTurno = jugadores[cantMovActual % jugadores.length]; // cambia el jugador
-        ventanaJuego.setTurnoJugador(jugadorTurno.getNombre()); //escribe el jugador en pantalla
-
+        Jugador jugadorTurno = jugadores[cantMovActual % jugadores.length];
+        ventanaJuego.setTurnoJugador(jugadorTurno.getNombre());
         return jugadorTurno;
     }
 
-    /**
-     * Simula el giro de la ruleta e imprime el resultado obtenido.
-     * @return el número de posiciones que el jugador deberá moverse en el tablero.
-    */
-    public int girarRuleta( ) {
+    public int girarRuleta() {
         return this.ventanaJuego.girarRuletaSync();
     }
 
-    public void accionAmarilla(Jugador jugadorTurno,int nivel) {
+    public void accionAmarilla(Jugador jugadorTurno, int nivel) {
 
-        // todo acción random (quizas girar ruleta y que te pueda tocar cualquier carta)
-        VentanaCartaAmarilla.llamarVentanaAmarilla();
-
+        VentanaCarta.mostrarCartaInformativa("Carta Amarilla", "Te tocó carta amarilla", "Debes girar la ruleta (1 a 5)", PantallaColor.AMARILLO);
         switch (girarRuleta() % 5) {
-            case 1:
-                // accion azul
-                accionAzul(jugadorTurno, nivel);
-                break;
-            case 2:
-                // accion roja
-                accionRoja();
-                break;
-            case 3:
-                //accion verde
-                accionVerde(jugadorTurno);
-                break;
-            case 4:
-                // accion rosa
-                accionRosa();
-                break;
-            case 5:
-                // accion naranja
-                accionNaranja(jugadorTurno,nivel);
-                break;
+            case 1: accionAzul(jugadorTurno, nivel);
+            break;
+            case 2: accionRoja(jugadorTurno);
+            break;
+            case 3: accionVerde(jugadorTurno);
+            break;
+            case 4: accionRosa();
+            break;
+            case 5: accionNaranja(jugadorTurno, nivel);
+            break;
         }
     }
 
-    /**
-     * Permite al jugador elegir entre dos profesiones del mismo nivel.
-     * Genera dos opciones aleatorias, muestra una ventana de elección
-     * y actualiza la profesión del jugador según su seleccion.
-     */
     public void accionAzul(Jugador jugadorTurno, int nivel) {
-
-        // Crear la carta y obtener las dos profesiones aleatorias
         CartaAzul profesion = new CartaAzul();
         List<Integer> ids = profesion.obtenerRandom(nivel);
 
-        // Buscar las dos cartas por ID
         CartaAzul profesion1 = CartaAzul.buscarCartaId(ids.get(0));
         CartaAzul profesion2 = CartaAzul.buscarCartaId(ids.get(1));
 
-        // Crear las descripciones para mostrar en la ventana
-        String[] desc1 = { "Profesión: " + profesion1.getTitulo(), "Salario: " + profesion1.getSueldo(), "Nivel: " + profesion1.getNivel()};
-        String[] desc2 = {"Profesión: " + profesion2.getTitulo(), "Salario: " + profesion2.getSueldo(), "Nivel: " + profesion2.getNivel()};
+        String[][] opciones = {
+                { "Profesión: " + profesion1.getTitulo(), "Salario: " + profesion1.getSueldo(), "Nivel: " + profesion1.getNivel() },
+                { "Profesión: " + profesion2.getTitulo(), "Salario: " + profesion2.getSueldo(), "Nivel: " + profesion2.getNivel() }
+        };
 
-        // Mostrar la ventana de elección
-        VentanaCartaAzul ventana = new VentanaCartaAzul(desc1, desc2);
+        VentanaCarta ventana = new VentanaCarta("Elige una profesión", PantallaColor.AZUL, opciones);
         int seleccion = ventana.mostrarYEsperarSeleccion();
 
-        if (seleccion == 1){
-            jugadorTurno.actualizar("id_profesion",ids.get(0).intValue());
+        if (seleccion == 1) {
+            jugadorTurno.actualizar("id_profesion", ids.get(0));
             ventanaJuego.actualizarProfesionJugador(jugadorTurno.getId(), profesion1.getTitulo());
-        }
-        if (seleccion == 2){
-            jugadorTurno.actualizar("id_profesion",ids.get(1).intValue());
+        } else if (seleccion == 2) {
+            jugadorTurno.actualizar("id_profesion", ids.get(1));
             ventanaJuego.actualizarProfesionJugador(jugadorTurno.getId(), profesion2.getTitulo());
         }
     }
 
-    public void accionRoja(){
-        VentanaCartaRoja.llamarVentanaRoja();
-        // todo acción de pagar impuesto
-    }
+    public void accionRoja(Jugador jugadorTurno) {
+        VentanaCarta.mostrarCartaInformativa("Carta Roja", "Te tocó carta roja", "Debes pagar impuestos", PantallaColor.ROJO);
 
-    public void accionVerde(Jugador jugadorTurno){
-
-        VentanaCartaVerde.llamarVentanaVerde();
-        //Traigo el patrimonio actual y lo sumo al bono y luego atualizo, porque la funcion jugador.actualizar reemplaza un valor por otro
         int patrimonioActual = jugadorTurno.getPatrimonio();
-        int bono= patrimonioActual + 1000;
-        //Actualizo el patrimonio
-        jugadorTurno.actualizar("patrimonio",bono);
-        // todo acción de cobrar bono
-        // todo ya se cobro el sueldo normal cada vez que se paso por "arriba" de una casilla verde
-
+        int impuesto = patrimonioActual - 1000;
+        jugadorTurno.actualizar("patrimonio", impuesto);
     }
-    public void accionRosa(){
 
-        String[] mascota = {"Mascota: Perro", "Edad: 2 años", "Raza: Labrador"}; // codigo de prueba
-        String[] pareja = {"Pareja: Ana", "Edad: 28", "Profesión: Abogada"};// codigo de prueba
-        String[] hijo = {"Hijo: Juan", "Edad: 5", "Escuela: Primaria"};// codigo de prueba
+    public void accionVerde(Jugador jugadorTurno) {
+        VentanaCarta.mostrarCartaInformativa("Carta Verde", "Te tocó carta verde", "Cobra bono", PantallaColor.VERDE);
 
-        VentanaCartaRosa ventana = new VentanaCartaRosa(mascota, pareja, hijo);// codigo de prueba
-        int eleccion = ventana.mostrarYEsperarSeleccion();// codigo de prueba
+        int patrimonioActual = jugadorTurno.getPatrimonio();
+        int bono = patrimonioActual + 1000;
+        jugadorTurno.actualizar("patrimonio", bono);
+    }
+
+    public void accionRosa() {
+
+        String[][] opcionesRosa = {
+                {"Mascota: Perro", "Edad: 2 años", "Raza: Labrador"},
+                {"Pareja: Ana", "Edad: 28", "Profesión: Abogada"},
+                {"Hijo: Juan", "Edad: 5", "Escuela: Primaria"}};
+
+        VentanaCarta ventana = new VentanaCarta("Elige tu opción", PantallaColor.ROSA, opcionesRosa);
+        int eleccion = ventana.mostrarYEsperarSeleccion();
+
 
 
         // todo acción de que te toque familia
-        // todo falta crear casillas rosas en la bd
-    }
-    public void accionNaranja(Jugador jugadorTurno, int nivel){
 
-        // Crear la carta y obtener las dos profesiones aleatorias
+
+    }
+
+    public void accionNaranja(Jugador jugadorTurno, int nivel) {
         CartaNaranja casa = new CartaNaranja();
         List<Integer> ids = casa.obtenerRandom(nivel);
 
-        // Buscar las dos cartas por ID
         CartaNaranja casa1 = CartaNaranja.buscarCartaId(ids.get(0));
         CartaNaranja casa2 = CartaNaranja.buscarCartaId(ids.get(1));
 
-        // Crear las descripciones para mostrar en la ventana
-        String[] desc1 = { "Propiedad : " + casa1.getDescripcion(), "Precio compra: " + casa1.getPrecio_compra(), "Precio venta: " + casa1.getPrecio_venta()};
-        String[] desc2 = {"Propiedad: " + casa2.getDescripcion(), "Precio compra: " + casa2.getPrecio_compra(), "Precio venta: " + casa2.getPrecio_venta()};
+        String[][] opciones = {
+                {"Propiedad: " + casa1.getDescripcion(), "Precio compra: " + casa1.getPrecio_compra(), "Precio venta: " + casa1.getPrecio_venta()},
+                {"Propiedad: " + casa2.getDescripcion(), "Precio compra: " + casa2.getPrecio_compra(), "Precio venta: " + casa2.getPrecio_venta()}
+        };
 
-        VentanaCartaNaranja ventana = new VentanaCartaNaranja(desc1,desc2);//
+        VentanaCarta ventana = new VentanaCarta("Elige una casa", PantallaColor.NARANJA, opciones);
         int seleccion = ventana.mostrarYEsperarSeleccion();
 
         if (seleccion == 1){
@@ -321,11 +244,6 @@ public class Juego {
             ventanaJuego.actualizarProfesionJugador(jugadorTurno.getId(), casa2.getDescripcion());
         }
 
-
-
-
-        // todo no se si esta la casilla naranaj en el juego original
-        // todo accion de elegir entre dos casas de un mismo nivel (podria ser que primero te de nivel 1, luego nievl 2... creciendo)
     }
 
     private void borrarDatosBDDinamica() {
--- src/main/java/org/example/MainTest1.java
+++ src/main/java/org/example/MainTest1.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.utils.UserInput;
 
 public class MainTest1 {
 
